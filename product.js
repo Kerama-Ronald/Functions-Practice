5675f378a0f1f75dfcdef6159beb90e9880ffb88
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-03-15 11:26:25 +0300

    longest string

---
+++ product.js
@@ -0,0 +1,30 @@
+
+// function getProduct(arr){
+
+//     let product = 1;
+
+//     for(let i = 0; i < arr.length; i++){
+
+//         product = product * arr[i];
+//     }
+
+//     return product;
+
+// }
+
+// console.log(getProduct([2, 6, 8, 9]))
+
+
+getProduct = (arr) => {
+
+    let product = 1;
+
+    for(let i = 0; i < arr.length; i++){
+
+        product = product * arr[i];
+    }
+
+    return product;
+
+}
+console.log(getProduct([2, 6, 8, 9]))
