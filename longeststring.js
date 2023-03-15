7f4c5fc35fe1f9ad87d7b8a4ba72924e83e7b558
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-03-15 11:38:00 +0300

    longest string function

--- longeststring.js
+++ longeststring.js
@@ -1,17 +1,44 @@
-function longestString(str){
+// function longestString(strings){
 
-    var strSplit = str.split(' ');
-    var longestWord = 0;
-    for(var i = 0; i < strSplit.length; i++){
-    if(strSplit[i].length > longestWord){
-	longestWord = strSplit[i].length;
-    
-    
+//     //split the words/strings into individual words
+//     var strSplit = strings.split(' '); //.split is a method to split the words
 
+//     //initialize the variable longestWord and give it a value 
+//     var longestWord = 0;
+
+//     //loop over the strings/words to find the longest word
+//     for(var i = 0; i < strSplit.length; i++){
+
+//         if(strSplit[i].length > longestWord){
+//         longestWord = strSplit[i].length;
+    
+//      }
+//     }
+
+//     return longestWord;
+//     }
+
+
+// console.log(longestString("Medrine Jepkemoi"))
+
+longestString = (strings) => {
+     //split the words/strings into individual words
+     var strSplit = strings.split(' '); //.split is a method to split the words
+
+     //initialize the variable longestWord and give it a value 
+     var longestWord = 0;
+ 
+     //loop over the strings/words to find the longest word
+     for(var i = 0; i < strSplit.length; i++){
+ 
+         if(strSplit[i].length > longestWord){
+         longestWord = strSplit[i].length;
+     
+      }
      }
-    }
-  return longestWord;
+ 
+     return longestWord;
+
 }
 
-
-console.log(longestString("Medrine Jepkemoi"))
+console.log(longestString("I love eating"))
