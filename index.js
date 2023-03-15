4ccd257008d252e95890a2bf3be3ed0058680a12
+ <+>
2023-03-15 11:27:54 +0300

    no.4

---
+++ index.js
@@ -0,0 +1,27 @@
+// function sumArray(numbers) {
+//     let sum = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//       sum += numbers[i];
+//     }
+//     return sum;
+//   }
+  
+//   let numbersArray = [190, 90];
+//   let total = sumArray(numbersArray);
+//   console.log(total); 
+
+
+  function getEvennumbers(num){
+    const evennum = [];
+    for(let i = 0; i< num.length; i++){
+        if(num[i] % 2 === 0) {
+            evennum.push(num[i]);
+        }
+    }
+     return evennum;
+}
+
+const num = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const evenNum = getEvennumbers(num);
+console.log(evenNum);
+  
